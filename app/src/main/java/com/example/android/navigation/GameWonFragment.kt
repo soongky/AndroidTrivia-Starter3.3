6ad4ef8b3497e8a1480ac15814cb584987690150
Soong Kah Yee <soongky-am17@student.tar.edu.my>
2019-12-08 23:37:49 +0800

    3.3

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -20,8 +20,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentGameWonBinding
 
 
@@ -31,6 +33,25 @@ class GameWonFragment : Fragment() {
         // Inflate the layout for this fragment
         val binding: FragmentGameWonBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_game_won, container, false)
+        val args = GameWonFragmentArgs.fromBundle(arguments!!)
+        Toast.makeText(context, "NumCorrect: ${args.numCorrect}, NumQuestions: ${args.numQuestions}", Toast.LENGTH_LONG).show()
+        binding.nextMatchButton.setOnClickListener { view: View ->
+            view.findNavController()
+                    .navigate(GameWonFragmentDirections.actionGameWonFragmentToGameFragment())
+        }
+
+        setHasOptionsMenu(true)
         return binding.root
+    }
+        private fun getShareIntent() : Intent {
+            val args = GameWonFragmentArgs.fromBundle(arguments!!)
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.setType("text/plain")
+                    .putExtra(Intent.EXTRA_TEXT, getString(R.string.share_success_text, args.numCorrect, args.numQuestions))
+            return shareIntent
+
+    }
+    private fun shareSuccess() {
+        startActivity(getShareIntent())
     }
 }
